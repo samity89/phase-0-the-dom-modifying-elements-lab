8fe78014e8fb56bd7c6a438dfb6462e54c98d9ee
Samuel Amity <sam.amity@gmail.com>
2022-06-18 21:12:47 -0400

    commit tree 07edee71f857ba5fd032c275bb98d2229cd283ff

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+document.querySelector("main").remove();
+
+var newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+
+newHeader.innerHTML =
+  "<h1>Samity is the champion</h1>";
